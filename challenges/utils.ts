f8d37e71bbd81a6494afcebfa850ae1be68a35a6
Siedemus <barsak1177@gmail.com>
2024-12-01 17:39:41 +0100

    add sum util

--- challenges/utils.ts
+++ challenges/utils.ts
@@ -2,3 +2,5 @@ import { readFileSync } from "fs";
 
 export const loadInput = (file: string): string =>
   readFileSync(file, { encoding: "utf-8" });
+
+export const sum = (arr: number[]) => arr.reduce((acc, el) => acc + el, 0);
