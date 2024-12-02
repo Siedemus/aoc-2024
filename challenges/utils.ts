a027aab50e47c86f007154a8c6eba0a8e0a86180
Siedemus <barsak1177@gmail.com>
2024-12-02 12:35:44 +0100

    add difference util

--- challenges/utils.ts
+++ challenges/utils.ts
@@ -4,3 +4,7 @@ export const loadInput = (file: string): string =>
   readFileSync(file, { encoding: "utf-8" });
 
 export const sum = (arr: number[]) => arr.reduce((acc, el) => acc + el, 0);
+
+export const difference = (a: number, b: number) => {
+  return Math.abs(a - b);
+};
