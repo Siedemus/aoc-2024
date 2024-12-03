1b32b28684abbfea77c506b4ccb15cb112fa675b
Siedemus <barsak1177@gmail.com>
2024-12-03 23:21:24 +0100

    refactored version of 1 level

--- challenges/day-03/index1.ts
+++ challenges/day-03/index1.ts
@@ -2,17 +2,9 @@ import { loadInput, sum } from "../utils";
 
 const input = loadInput("challenges/day-03/input");
 
-const regex = /mul\((\d{1,3}),(\d{1,3})\)/g;
-const results = [];
-let match;
+const matches = input.matchAll(new RegExp(/mul\((\d{1,3}),(\d{1,3})\)/g));
+const results = Array.from(matches).map(([, A, B]) => {
+  return Number(A) * Number(B);
+});
 
-while ((match = regex.exec(input)) !== null) {
-  const A = Number(match[1]);
-  const B = Number(match[2]);
-
-  results.push(A * B);
-}
-
-const finalResult = sum(results);
-
-console.log(finalResult);
+console.log(sum(results));
