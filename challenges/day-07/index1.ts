31ac01955c76469492d0fcb1513ad5fddc42376f
Siedemus <barsak1177@gmail.com>
2024-12-08 20:11:50 +0100

    level 1 done

---
+++ challenges/day-07/index1.ts
@@ -0,0 +1,43 @@
+import { loadInput, sum } from "../utils";
+
+const input = loadInput("challenges/day-07/input");
+
+const validResults: number[] = [];
+
+const equations = input
+  .split(/\n/)
+  .map((row) => row.trim().split(": "))
+  .map((row) => ({
+    result: Number(row[0]),
+    numbers: row[1].split(" ").map(Number),
+  }));
+
+const canAchieveTarget = (numbers: number[], target: number) => {
+  const stack: Array<[number, number]> = [];
+  stack.push([0, numbers[0]]);
+
+  while (stack.length > 0) {
+    const [index, current] = stack.pop()!;
+
+    if (index === numbers.length - 1) {
+      if (current === target) {
+        return true;
+      }
+      continue;
+    }
+
+    const nextNumber = numbers[index + 1];
+    stack.push([index + 1, current + nextNumber]);
+    stack.push([index + 1, current * nextNumber]);
+  }
+};
+
+for (const equation of equations) {
+  const targetAchieved = canAchieveTarget(equation.numbers, equation.result);
+  if (targetAchieved) {
+    validResults.push(equation.result);
+  }
+}
+
+const result = sum(validResults);
+console.log(result);
